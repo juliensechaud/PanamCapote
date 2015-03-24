948a70a97da72a45e3b8242c1f36cb04c11cf33e
Julien SECHAUD <juliensechaud@gmail.com>
2015-03-24 22:47:22 +0100

    Initial Commit

---
+++ parisCapote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  parisCapote
+//
+//  Created by Julien SECHAUD on 24/03/2015.
+//  Copyright (c) 2015 ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
